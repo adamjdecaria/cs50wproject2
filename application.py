de104446da928ee51dcaadd9c63ef8cdbcae0054
adam decaria <adamdecaria@outlook.com>
2019-11-09 22:06:10 -0500

    create channel function working

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, flash, render_template
+from flask import Flask, session, flash, render_template, request
 from flask_session import Session
 from flask_socketio import SocketIO, emit
 
@@ -11,6 +11,19 @@ socketio = SocketIO(app)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# list of channels; initialize with one "main" channel
+channels = []
+channels.append("main")
+
 @app.route("/")
 def index():
-    return render_template("flack.html")
+    return render_template("flack.html", channels=channels)
+
+@app.route("/createChannel", methods=["POST"])
+def createChannel():
+    """Create a new chat channel"""
+    channel = request.form.get("newchannel")
+    channels.append(channel)
+    print(f"ADDED NEW CHANNEL: {channel}")
+    
+    return render_template("flack.html", channels=channels)
